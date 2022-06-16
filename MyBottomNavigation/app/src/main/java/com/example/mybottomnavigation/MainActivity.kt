20030a45001a7256f074db5d3338ccfcd922d842
Prih Diantono Abda`u <79685285+abdaupd@users.noreply.github.com>
2022-06-16 10:42:24 +0700

    061622
    061622

--- MyBottomNavigation/app/src/main/java/com/example/mybottomnavigation/MainActivity.kt
+++ MyBottomNavigation/app/src/main/java/com/example/mybottomnavigation/MainActivity.kt
@@ -24,12 +24,12 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        val appBarConfiguration = AppBarConfiguration.Builder(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
